40acd1f22753066e2076863ae80f2d5daefa6021
Tonton-Blax <redval@gmail.com>
2020-08-31 15:15:34 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#Sapper with SASS + BULMA
+
+# sapper-template
+
+The default [Sapper](https://github.com/sveltejs/sapper) template, embedded with bulma, sass and a couple of bulma addons.
+That's it.
+That's the md.
---
+++ src/routes/_layout.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Nav from "../components/Nav.svelte";
+
+  export let segment;
+</script>
+
+<style lang="scss" global>
+  @import "../style/global.sass";
+  main {
+    position: relative;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+</style>
+
+<Nav {segment} />
+
+<main>
+  <slot />
+</main>
